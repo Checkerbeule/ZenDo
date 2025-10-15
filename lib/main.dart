761ad6d613cc4289587a8f5f0ae50674f541fc4d
Robin Schäfer <schaefer.robin1@gmail.com>
2025-10-15 10:22:40 +0200

    Wechsel zwischen den Listen mittels TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:zen_do/todo_list_page.dart';
-import 'package:zen_do/model/todo.dart';
 import 'package:zen_do/model/todo_list.dart';
 import 'package:zen_do/model/todo_scope.dart';
+import 'package:zen_do/todo_list_page.dart';
 
 void main() {
   runApp(const ZenDoApp());
@@ -36,7 +35,6 @@ class ZenDoAppState extends ChangeNotifier {
   void notify() {
     notifyListeners();
   }
-
 }
 
 class ZenDoHomePage extends StatelessWidget {
@@ -46,12 +44,32 @@ class ZenDoHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     ZenDoAppState state = context.watch<ZenDoAppState>();
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('ZenDo'),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('ZenDo Listen'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.today), text: 'Täglich'),
+              Tab(icon: Icon(Icons.calendar_month), text: 'Wöchentlich'),
+              Tab(icon: Icon(Icons.calendar_today), text: 'Jährlich'),
+              Tab(icon: Icon(Icons.format_list_bulleted), text: 'Backlog'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            TodoListPage(list: state.dailyToDoList),
+            TodoListPage(list: state.weeklyToDoList),
+            TodoListPage(list: state.yearlyToDoList),
+            TodoListPage(list: state.backlog),
+          ],
+        ),
       ),
-      body: TodoListPage(list: state.dailyToDoList),
     );
   }
 }
