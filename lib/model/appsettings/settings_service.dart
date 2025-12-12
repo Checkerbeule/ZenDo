0a8997e5ed696fc07923458ec6d8752c466152e2
Robin Schäfer <schaefer.robin1@gmail.com>
2025-12-12 20:41:26 +0100

    further implementation of SettingsService

--- lib/model/appsettings/settings_service.dart
+++ lib/model/appsettings/settings_service.dart
@@ -7,6 +7,11 @@ abstract class SettingsService {
   Future<void> saveSortOrder(ListScope scope, SortOrder sortOrder);
   Future<SortOption?> getSortOption(ListScope scope);
   Future<SortOrder?> getSortOrder(ListScope scope);
+
+  Future<void> saveActiveListScopes(Set<ListScope> activeScopes);
+  Future<Set<ListScope>?> getActiveListScopes();
+  Future<void> addActiveScope(ListScope activeScope);
+  Future<void> removeActiveScope(ListScope activeScope);
 }
 
 class SharedPrefsSettingsService implements SettingsService {
@@ -24,54 +29,105 @@ class SharedPrefsSettingsService implements SettingsService {
 
   late final SharedPreferences prefs;
 
-  Map<ListScope, SortOption> sortOptions = {};
-  Map<ListScope, SortOrder> sortOrders = {};
+  final Map<ListScope, SortOption> _sortOptions = {};
+  final Map<ListScope, SortOrder> _sortOrders = {};
 
-  String _getSortOptionPrefKey(ListScope scope) {
-    return 'todo.${scope.name}.list.sortOption';
-  }
+  Set<ListScope>? _activeListScopes;
 
-  String _getSortOrderPrefKey(ListScope scope) {
-    return 'todo.${scope.name}.list.sortOrder';
-  }
+  String _getSortOptionPrefKey(ListScope scope) =>
+      'todo.list.${scope.name}.sortOption';
+  String _getSortOrderPrefKey(ListScope scope) =>
+      'todo.list.${scope.name}.sortOrder';
+
+  static const String _activeListScopesPrefKey = 'todo.manager.activeScopes';
 
   @override
   Future<void> saveSortOption(ListScope scope, SortOption sortOption) async {
-    sortOptions[scope] = sortOption;
-    final prefs = await SharedPreferences.getInstance();
+    _sortOptions[scope] = sortOption;
     await prefs.setInt(_getSortOptionPrefKey(scope), sortOption.index);
   }
 
   @override
   Future<void> saveSortOrder(ListScope scope, SortOrder sortOrder) async {
-    sortOrders[scope] = sortOrder;
-    final prefs = await SharedPreferences.getInstance();
+    _sortOrders[scope] = sortOrder;
     await prefs.setInt(_getSortOrderPrefKey(scope), sortOrder.index);
   }
 
   @override
   Future<SortOption?> getSortOption(ListScope scope) async {
-    if (sortOptions.containsKey(scope)) {
-      return sortOptions[scope];
+    if (_sortOptions.containsKey(scope)) {
+      return _sortOptions[scope];
     }
-    final prefs = await SharedPreferences.getInstance();
     final index = prefs.getInt(_getSortOptionPrefKey(scope));
-    if (index != null) {
-      return SortOption.values[index];
-    }
-    return null;
+    return index == null ? null : SortOption.values[index];
   }
 
   @override
   Future<SortOrder?> getSortOrder(ListScope scope) async {
-    if (sortOrders.containsKey(scope)) {
-      return sortOrders[scope];
+    if (_sortOrders.containsKey(scope)) {
+      return _sortOrders[scope];
     }
-    final prefs = await SharedPreferences.getInstance();
     final index = prefs.getInt(_getSortOrderPrefKey(scope));
-    if (index != null) {
-      return SortOrder.values[index];
+    return index == null ? null : SortOrder.values[index];
+  }
+
+  @override
+  Future<void> saveActiveListScopes(Set<ListScope> activeScopes) async {
+    _activeListScopes = {...activeScopes};
+    await prefs.setStringList(
+      _activeListScopesPrefKey,
+      _toScopeNameList(activeScopes),
+    );
+  }
+
+  @override
+  Future<Set<ListScope>?> getActiveListScopes() async {
+    if (_activeListScopes != null) return _activeListScopes;
+    final List<String>? scopeNames = prefs.getStringList(
+      _activeListScopesPrefKey,
+    );
+    if (scopeNames != null) {
+      _activeListScopes = _toListScopeSet(scopeNames);
     }
-    return null;
+    return _activeListScopes;
+  }
+
+  @override
+  Future<void> addActiveScope(ListScope activeScope) async {
+    _activeListScopes ??= {};
+    if (!_activeListScopes!.contains(activeScope)) {
+      _activeListScopes!.add(activeScope);
+      await prefs.setStringList(
+        _activeListScopesPrefKey,
+        _toScopeNameList(_activeListScopes!),
+      );
+    }
+  }
+
+  @override
+  Future<void> removeActiveScope(ListScope scope) async {
+    if (_activeListScopes != null) {
+      _activeListScopes!.remove(scope);
+      await prefs.setStringList(
+        _activeListScopesPrefKey,
+        _toScopeNameList(_activeListScopes!),
+      );
+    }
+  }
+
+  List<String> _toScopeNameList(Set<ListScope> scopes) {
+    final List<String> scopeNames = [];
+    for (final s in scopes) {
+      scopeNames.add(s.name);
+    }
+    return scopeNames;
+  }
+
+  Set<ListScope> _toListScopeSet(List<String> names) {
+    final Set<ListScope> scopeSet = {};
+    for (final n in names) {
+      scopeSet.add(ListScope.values.byName(n));
+    }
+    return scopeSet;
   }
 }
