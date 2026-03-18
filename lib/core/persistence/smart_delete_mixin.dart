6214839d117ef2f669993647103db5ae2af9b5de
Robin Schäfer <schaefer.robin1@gmail.com>
2026-03-18 13:36:47 +0100

    remove try catch on smartDeleteMixin

--- lib/core/persistence/smart_delete_mixin.dart
+++ lib/core/persistence/smart_delete_mixin.dart
@@ -9,27 +9,15 @@ mixin SmartDeleteMixin {
     required TableInfo<T, D> table,
     required D entity,
   }) async {
-    try {
-      if (entity.syncStatus == SyncStatus.localOnly) {
-        await (db.delete(table)..where((t) => t.uuid.equals(entity.uuid))).go();
-      } else {
-        await (db.update(
-          table,
-        )..where((t) => t.uuid.equals(entity.uuid))).write(
-          RawValuesInsertable({
-            'sync_status': Variable(SyncStatus.deleted.index),
-            'updated_at': Variable(DateTime.now()),
-          }),
-        );
-      }
-    } on NoSuchMethodError catch (e) {
-      throw ArgumentError(
-        "Entity $D is not a Syncable. Sync fields missing: $e",
+    if (entity.syncStatus == SyncStatus.localOnly) {
+      await (db.delete(table)..where((t) => t.uuid.equals(entity.uuid))).go();
+    } else {
+      await (db.update(table)..where((t) => t.uuid.equals(entity.uuid))).write(
+        RawValuesInsertable({
+          'sync_status': Variable(SyncStatus.deleted.index),
+          'updated_at': Variable(DateTime.now()),
+        }),
       );
-    } on TypeError catch (e) {
-      throw ArgumentError("Entity $D has wrong types on sync fields: $e");
-    } catch (e) {
-      rethrow;
     }
   }
 }
