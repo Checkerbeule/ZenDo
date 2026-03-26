f434c4794338b036a7df3863d429257616481786
Robin Schäfer <schaefer.robin1@gmail.com>
2026-03-26 15:20:25 +0100

    refactored buttons and bottomSheet

--- lib/features/tags/ui/tag_edit_sheet.dart
+++ lib/features/tags/ui/tag_edit_sheet.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:zen_do/core/persistence/app_database.dart';
+import 'package:zen_do/core/ui/base_bottom_sheet.dart';
+import 'package:zen_do/core/ui/standard_buttons.dart';
 import 'package:zen_do/features/tags/data/tag_repository.dart';
 import 'package:zen_do/features/tags/ui/tag_widget.dart';
 
@@ -70,135 +72,69 @@ class _TagEditSheetState extends State<TagEditSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height * 0.92,
-        ),
-        child: Padding(
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-            left: 16,
-            right: 16,
-            top: 16,
+    return BaseBottomSheet(
+      header: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.initialTag == null
+                ? context.tagsL10n.addNewTag
+                : context.tagsL10n.editTag,
+            style: TextTheme.of(context).headlineSmall,
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Draghandle
-              Center(
-                child: Container(
-                  width: 40,
-                  height: 3,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(2),
-                  ),
-                ),
-              ),
-              Text(
-                widget.initialTag == null
-                    ? context.tagsL10n.addNewTag
-                    : context.tagsL10n.editTag,
-                style: TextTheme.of(context).headlineSmall,
-              ),
-              const SizedBox(height: 2),
-              TagWidget.preview(
-                name: _nameController.text,
-                colorValue: _selectedColor.toARGB32(),
-              ),
-              const SizedBox(height: 5),
-
-              // Body
-              Flexible(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: _nameController,
-                        decoration: InputDecoration(
-                          labelText: context.tagsL10n.tagNameLabel,
-                        ),
-                        autofocus: true,
-                        maxLength: 25,
-                        textCapitalization: TextCapitalization.sentences,
-                        onChanged: (_) => setState(() {}),
-                      ),
-
-                      Container(
-                        constraints: const BoxConstraints(
-                          minHeight: 460,
-                          minWidth: 300,
-                          maxWidth: 380,
-                        ),
-                        child: ColorPicker(
-                          color: _selectedColor,
-                          onColorChanged: (Color color) => setState(() {
-                            _selectedColor = color;
-                          }),
-                          pickersEnabled: const <ColorPickerType, bool>{
-                            ColorPickerType.primary: true,
-                            ColorPickerType.accent: false,
-                            ColorPickerType.wheel: true,
-                          },
-                          pickerTypeLabels: {
-                            ColorPickerType.primary:
-                                context.tagsL10n.colorPickerPrimaryLable,
-                            ColorPickerType.wheel:
-                                context.tagsL10n.colorPickerWheelLable,
-                          },
-                          heading: Text(context.tagsL10n.tagColorHeading),
-                          subheading: Text(context.tagsL10n.tagColorSubheading),
-                          wheelSubheading: Text(
-                            context.tagsL10n.tagColorSubheading,
-                          ),
-                          showColorName: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-              // Footer
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      foregroundColor: Theme.of(context).colorScheme.error,
-                      backgroundColor: Theme.of(
-                        context,
-                      ).colorScheme.error.withValues(alpha: 0.15),
-                    ),
-                    child: Text(
-                      MaterialLocalizations.of(context).cancelButtonLabel,
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  const SizedBox(width: 5),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      foregroundColor: Theme.of(context).colorScheme.primary,
-                      backgroundColor: Theme.of(
-                        context,
-                      ).colorScheme.primaryContainer.withValues(alpha: 0.5),
-                    ),
-                    child: Text(
-                      MaterialLocalizations.of(context).saveButtonLabel,
-                    ),
-                    onPressed: () {
-                      _save();
-                    },
-                  ),
-                ],
-              ),
-            ],
+          const SizedBox(height: 2),
+          TagWidget.preview(
+            name: _nameController.text,
+            colorValue: _selectedColor.toARGB32(),
           ),
-        ),
+          const SizedBox(height: 5),
+        ],
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              labelText: context.tagsL10n.tagNameLabel,
+            ),
+            autofocus: true,
+            maxLength: 25,
+            textCapitalization: TextCapitalization.sentences,
+            onChanged: (_) => setState(() {}),
+          ),
+
+          Container(
+            constraints: const BoxConstraints(
+              minHeight: 460,
+              minWidth: 300,
+              maxWidth: 380,
+            ),
+            child: ColorPicker(
+              color: _selectedColor,
+              onColorChanged: (Color color) => setState(() {
+                _selectedColor = color;
+              }),
+              pickersEnabled: const <ColorPickerType, bool>{
+                ColorPickerType.primary: true,
+                ColorPickerType.accent: false,
+                ColorPickerType.wheel: true,
+              },
+              pickerTypeLabels: {
+                ColorPickerType.primary:
+                    context.tagsL10n.colorPickerPrimaryLable,
+                ColorPickerType.wheel: context.tagsL10n.colorPickerWheelLable,
+              },
+              heading: Text(context.tagsL10n.tagColorHeading),
+              subheading: Text(context.tagsL10n.tagColorSubheading),
+              wheelSubheading: Text(context.tagsL10n.tagColorSubheading),
+              showColorName: true,
+            ),
+          ),
+        ],
+      ),
+      footer: CancelSaveButtonPair(
+        onCancelPressed: () => Navigator.of(context).pop(),
+        onSavePressed: _save,
       ),
     );
   }
